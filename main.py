20777b502c5aa42c60ab9c654999a455cf9561ec
pichurr0 <99566614+pichurr0@users.noreply.github.com>
2022-09-14 00:13:51 -0400

    ok

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Esta es la prueba de que la aplicacion puede ser desplegada y que tu la vez desde la casa te amo"})
+    return jsonify({"amor mio":"Esta es la prueba de que la aplicacion puede ser desplegada y que tu la vez desde la casa te amo"})
 
 
 if __name__ == '__main__':
