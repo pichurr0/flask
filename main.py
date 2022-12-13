ba7d922f0bd015cf1190002d5327c5ba08f88c8b
pichurr0 <99566614+pichurr0@users.noreply.github.com>
2022-12-13 11:25:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return jsonify({"amor mio":"Esta es la prueba de que la aplicacion puede ser desplegada y que tu la vez desde la casa te amo"})
 
+@app.route('/home')
+def index():
+    return 'home'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
