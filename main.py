ad9eab81d748c2bcd52986afe0e8050b93ae7fbb
pichurr0 <99566614+pichurr0@users.noreply.github.com>
2022-12-14 13:07:45 -0500

    home

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return make_response("hola ue")
+
+
+@app.route('/home')
+def homex():
     return make_response({"key":"value"})
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=os.getenv("PORT", default=5000))
+    app.run(debug=True,host='0.0.0.0', port=os.getenv("PORT", default=10000))
