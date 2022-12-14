c872f901d176803f615cf5e0cbb0df0ac49d8aaa
pichurr0 <99566614+pichurr0@users.noreply.github.com>
2022-12-14 12:42:51 -0500

    host 0000

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+×import os
 from flask import Flask, make_response
 
 
@@ -9,4 +9,4 @@ def index():
     return make_response({"key":"value"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True,host='0.0.0.0', port=os.getenv("PORT", default=5000))
